20a8880a1ca309b5d9c3a34466925a7286528427
Yucca Lux <forgepixel@proton.me>
2025-09-30 06:36:11 +0200

    fix: add .js to istotyConfig import in ChatView.js

--- src/ChatView.js
+++ src/ChatView.js
@@ -2,7 +2,7 @@
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { useEffect, useMemo, useRef, useState } from "react";
 import claudeService from "./services/claudeService";
-import ISTOTY from "./istotyConfig";
+import ISTOTY from "./istotyConfig.js";
 import { zapiszRozmowe, odczytajRozmowe } from "./cryptoStorage";
 
 function ChatView() {
